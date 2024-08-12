d5213ceb95a8e4f6730ab8f930854223589d226a
Tomas Fernandez <tfernandez@buk.cl>
2024-08-12 10:48:33 -0400

    Code Smell: Switch Statements

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,13 +1,5 @@
 class GildedRose
 
-  MAX_QUALITY = 50
-  MIN_QUALITY = 0
-  QUALITY_CHANGE = 1
-  SELL_IN_EXPIRED = 0
-  SELL_IN_10_DAYS = 10
-  SELL_IN_5_DAYS = 5
-
-
   def initialize(items)
     @items = items
   end
@@ -16,61 +8,27 @@ class GildedRose
     @items.each do |item|
       case item.name
       when  "Aged Brie"
-        update_aged_brie(item)
+        AgedBrie.new(item).update
       when "Sulfuras, Hand of Ragnaros"
-        update_legendary_item(item)
+        LegendaryItem.new(item).update
       when "Backstage passes to a TAFKAL80ETC concert"
-        update_backstage_passes(item)
+        BackstagePass.new(item).update
       else
-        update_normal_item(item)
+        NormalItem.new(item).update
       end
-    end
-  end
-
-  private
-
-  def update_normal_item(item)
-    if item.quality > MIN_QUALITY
-      item.quality -= QUALITY_CHANGE
-    end
-    item.sell_in -= 1
-    if item.sell_in < SELL_IN_EXPIRED
-      if item.quality > MIN_QUALITY
-        item.quality = MIN_QUALITY
-      end
-    end
-  end
-
-  def update_aged_brie(item)
-    if item.quality < MAX_QUALITY
-      item.quality += QUALITY_CHANGE
-    end
-    item.sell_in -= 1
-    if item.sell_in < SELL_IN_EXPIRED
-      if item.quality < MAX_QUALITY
-        item.quality += QUALITY_CHANGE
-      end
-    end
-  end
-
-  def update_legendary_item(item)
-    # do nothing
-  end
-
-  def update_backstage_passes(item)
-    item.sell_in -= 1
-    if item.sell_in < SELL_IN_EXPIRED
-      item.quality = MIN_QUALITY
-    else
-      item.quality += QUALITY_CHANGE if item.quality < MAX_QUALITY
-      item.quality += QUALITY_CHANGE if item.sell_in < SELL_IN_10_DAYS && item.quality < MAX_QUALITY
-      item.quality += QUALITY_CHANGE if item.sell_in < SELL_IN_5_DAYS && item.quality < MAX_QUALITY
     end
   end
 end
 
 class Item
   attr_accessor :name, :sell_in, :quality
+
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+  QUALITY_CHANGE = 1
+  SELL_IN_EXPIRED = 0
+  SELL_IN_10_DAYS = 10
+  SELL_IN_5_DAYS = 5
 
   def initialize(name, sell_in, quality)
     @name = name
@@ -80,5 +38,72 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class NormalItem < Item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update()
+    if @item.quality > MIN_QUALITY
+      @item.quality -= QUALITY_CHANGE
+    end
+    @item.sell_in -= 1
+    if @item.sell_in < SELL_IN_EXPIRED
+      if @item.quality > MIN_QUALITY
+        @item.quality = MIN_QUALITY
+      end
+    end
+  end
+end
+
+class AgedBrie < Item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update()
+    if @item.quality < MAX_QUALITY
+      @item.quality += QUALITY_CHANGE
+    end
+    @item.sell_in -= 1
+    if @item.sell_in < SELL_IN_EXPIRED
+      if @item.quality < MAX_QUALITY
+        @item.quality += QUALITY_CHANGE
+      end
+    end
+  end
+end
+
+class LegendaryItem < Item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update()
+    # do nothing
+  end
+end
+
+class BackstagePass < Item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update()
+    @item.sell_in -= 1
+    if @item.sell_in < SELL_IN_EXPIRED
+      @item.quality = MIN_QUALITY
+    else
+      @item.quality += QUALITY_CHANGE if @item.quality < MAX_QUALITY
+      @item.quality += QUALITY_CHANGE if @item.sell_in < SELL_IN_10_DAYS && @item.quality < MAX_QUALITY
+      @item.quality += QUALITY_CHANGE if @item.sell_in < SELL_IN_5_DAYS && @item.quality < MAX_QUALITY
+    end
   end
 end
